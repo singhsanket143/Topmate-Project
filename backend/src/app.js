55eeae48a9b9acee9e635e5e7c31d18973112094
Sanket Singh <singhsanket143@gmail.com>
2024-12-20 20:14:19 +0530

    Added cors and cookie parser with request body parsing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,16 @@
 const express = require('express'); // Importing expressJS
+const cors = require('cors'); // Importing cors library
+const cookieParser = require('cookie-parser'); // Importing cookie-parser library
+
 
 // Create a new express app object
 const app = express();
+
+// Setup all the relevant middlewares
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Enable JSON parsing
+app.use(express.urlencoded({ extended: true })); // Enable URL-encoded parsing
+app.use(express.text()); // Enable text parsing
+app.use(cookieParser()); // Enable cookie parsing
 
 module.exports = app; // Exporting app object
